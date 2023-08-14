db826e431ad530a817fce71f55ef46f6298e5d0f
UmayrNordien <umayrnordien@gmail.com>
2023-08-14 09:01:25 +0200

    update: imported NavBar in Login Page :)

--- src/views/LoginFormView.vue
+++ src/views/LoginFormView.vue
@@ -1,14 +1,17 @@
 <template>
+  <LoginNavBar/>
     <div class="login-form-view">
       <LoginForm />
     </div>
   </template>
   
   <script>
+  import LoginNavBar from '@/components/LoginNavBar.vue'
   import LoginForm from '@/components/LoginForm.vue';
   
   export default {
     components: {
+      LoginNavBar,
       LoginForm,
     },
   };
